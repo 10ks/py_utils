b6cc73a20d0784aa8d6c765e806696a41323a2bd
Alex <pivoglot@gmail.com>
2020-01-25 20:28:35 +0200

    debugging

--- make_playlists.py
+++ make_playlists.py
@@ -1,31 +1,38 @@
 import os
 import os.path
 
-start_dir_name = "music"
-add_ext={'.mp3'}
+start_dir_name = "C:/DEV/GIT_Repos/py_utils/music"
+add_ext=['.mp3']  # list of file extensions which to include in playlist
 dir_data = os.walk(start_dir_name)
-top_dir = next(dir_data)
-# print(type(top_dir))
-# print(top_dir)
+# print(os.getcwd())
+
+# try:
+#     top_dir = next(dir_data)
+# except StopIteration as identifeier:
+#     print("No start directory. Exiting")
+#     exit()
+
 
 for root, dirs, files in dir_data:
-    # print(root)
-    # print(dirs)
-    # print(files)
-    # print("---")
+    print(root)
+    print(dirs)
+    print(files)
+    print("---")
     playlist = ""
     for f in files:
         ext = os.path.splitext(f)[1]
-        # print(ext)
         if ext in add_ext:
-            playlist += root[len(start_dir_name)+1:] + "\\" + f + "\r\n"   ## TODO remove base
+            # playlist += root[len(start_dir_name)+1:] + "\\" + f + "\r\n"
+            playlist += f + "\r\n"
     if playlist:
-        print("generated playlist:")
-        print(playlist)
+        # print("generated playlist:")
+        # print(playlist)
 
-        # TODO output playlist to separate dirs. Number sign is the problem
-
-        with open(root + "\\gen_play.txt", "w") as text_file:
+        # TODO Number sign in file name is the problem
+        # playlist_name = root + "\\" + root[0:4] + "play.m3u"
+        playlist_name = root + "\\" + "XXX" + "play.m3u"
+        print("playlist_name=", playlist_name)
+        with open(playlist_name, "w") as text_file:
             text_file.write(playlist)
     else:
         print("no playlist")
