51ff45e2a177171c3c99bf1d6d75849e69dd68bd
Alex <pivoglot@gmail.com>
2020-05-17 04:14:45 +0300

    experiments with camera settings

--- experiments/webcam/w1.py
+++ experiments/webcam/w1.py
@@ -7,13 +7,13 @@ from datetime import datetime
 vc = cv2.VideoCapture(1)
 
 vc.set(cv2.CAP_PROP_AUTOFOCUS, 0) # turn the autofocus off
-vc.set(cv2.CAP_PROP_FOCUS, 35)
+vc.set(cv2.CAP_PROP_FOCUS, 34)
+vc.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0)
+# print(vc.get(cv2.CAP_PROP_AUTO_EXPOSURE))
 # vc.set(cv2.CAP_PROP_WB_TEMPERATURE, 2400)
 # print(vc.get(cv2.CAP_PROP_WB_TEMPERATURE))
 vc.set(cv2.CAP_PROP_TEMPERATURE, 2800) #  2800 5500
 # print(vc.get(cv2.CAP_PROP_TEMPERATURE))
-vc.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0)
-# print(vc.get(cv2.CAP_PROP_AUTO_EXPOSURE))
 
 # vc.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0)
  
@@ -22,20 +22,26 @@ vc.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0)
 
 # vc.set(cv2.CV_CAP_PROP_SETTINGS, 0)
 
-# # with leds 
-# vc.set(cv2.CAP_PROP_EXPOSURE, -11) # -11 = minimum
-# vc.set(cv2.CAP_PROP_BRIGHTNESS, 30) # 30 = minimum
+# with leds 
+vc.set(cv2.CAP_PROP_EXPOSURE, -11) # -11 = minimum
+vc.set(cv2.CAP_PROP_BRIGHTNESS, 30) # 30 = minimum
 
 # # no led highlighting
 # vc.set(cv2.CAP_PROP_EXPOSURE, 1)
 # vc.set(cv2.CAP_PROP_BRIGHTNESS, 190)
 
-# lamp highlighting
-vc.set(cv2.CAP_PROP_EXPOSURE, -4)
-vc.set(cv2.CAP_PROP_BRIGHTNESS, 30)
+# # lamp highlighting
+# vc.set(cv2.CAP_PROP_EXPOSURE, -4)
+# vc.set(cv2.CAP_PROP_BRIGHTNESS, 30)
 
-vc.set(cv2.CAP_PROP_FRAME_WIDTH, 1280) # set the Horizontal resolution
-vc.set(cv2.CAP_PROP_FRAME_HEIGHT, 720) # Set the Vertical resolution
+# 160x120 320x240 640x480 800x600 1280x720
+# vc.set(cv2.CAP_PROP_FRAME_WIDTH, 1280) # set the Horizontal resolution
+# vc.set(cv2.CAP_PROP_FRAME_HEIGHT, 720) # Set the Vertical resolution
+vc.set(cv2.CAP_PROP_FRAME_WIDTH, 800) # set the Horizontal resolution
+vc.set(cv2.CAP_PROP_FRAME_HEIGHT, 600) # Set the Vertical resolution
+# print(vc.get(cv2.CAP_PROP_FRAME_WIDTH))
+# print(vc.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
 
 # print(vc.get(cv2.CAP_PROP_FOCUS))
 # print(vc.get(cv2.CAP_PROP_AUTO_EXPOSURE))
@@ -44,18 +50,18 @@ vc.set(cv2.CAP_PROP_FRAME_HEIGHT, 720) # Set the Vertical resolution
 while vc.isOpened():
     ret, image = vc.read()
     if ret:
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         cv2.imshow('webcam', image)
-        cv2.imshow('webcam_gr', gray)
-        cv2.moveWindow('webcam', 0, 0)
+        # cv2.imshow('webcam_gr', gray)
+        # cv2.moveWindow('webcam', 0, 0)
         # cv2.moveWindow('webcam_gr', 100, 100)
 
         # cv2.setWindowProperty('webcam', cv2.WND_PROP_AUTOSIZE, cv2.WINDOW_FULLSCREEN)
-
-        key = cv2.waitKey(1000) & 0xFF
+        key = cv2.waitKey(1) & 0xFF
         if key == ord("q"):
             break
         elif key == ord("s"):
+            # TODO add millisecond?
             filename = datetime.today().strftime('snapshot_%Y_%m_%d__%H_%M_%S.png')
             cv2.imwrite(filename, image)
             print(f"wrote {filename}")
